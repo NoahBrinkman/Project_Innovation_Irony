028f8571ec1ff37b7dfd0fc1637f7cf34a50fc9f
Noah Brinkman <noahpbrinkman@gmail.com>
2023-04-12 10:30:29 +0200

    GameEndEvent being sent

--- Irony_ServerApp/Assets/PlayerLobbyHandler.cs
+++ Irony_ServerApp/Assets/PlayerLobbyHandler.cs
@@ -119,7 +119,7 @@ public class PlayerLobbyHandler : MonoBehaviour
             timerText.text = string.Format("{0:00} : {1:00}",minutes,seconds);
             if (timer + bufferSeconds <= 0)
             {
-                
+                GameConnecter.Instance.EndGame();
             }
         }
     }
