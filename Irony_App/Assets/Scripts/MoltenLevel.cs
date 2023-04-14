b2114a7861786f8ca01121375277176bd23732d3
Gaming2003 <delanozwerink@gmail.com>
2023-04-14 10:57:05 +0200

    Reworked Smithing Scene
    Created Scene
    Added Camera Composition plugin

--- Irony_App/Assets/Scripts/MoltenLevel.cs
+++ Irony_App/Assets/Scripts/MoltenLevel.cs
@@ -4,6 +4,10 @@ using Adobe.Substance.Runtime;
 public class MoltenLevel : MonoBehaviour
 {
     public SubstanceRuntimeGraph mySubstance;
+    [SerializeField] private float BrightTimer = 1;
+    private float currentTimer = 0f;
+    [HideInInspector] public float SubstanceBrightness = 0.0f;
+    bool toBright;
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +17,12 @@ public class MoltenLevel : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && SubstanceBrightness <= 1)
+        {
+            toBright = true;
+        }
+
+        if (toBright)
         {
             UpdateSubstance();
         }
@@ -21,7 +30,17 @@ public class MoltenLevel : MonoBehaviour
 
     public void UpdateSubstance()
     {
-        mySubstance.SetInputFloat("moltenlevel" , 0.7f);
+        float brightnessDiff = 1 - SubstanceBrightness;
+
+        
+        if(currentTimer < BrightTimer)
+        {
+            SubstanceBrightness += 0.1f;
+            currentTimer += Time.deltaTime;
+        }
+
+
+        mySubstance.SetInputFloat("moltenlevel" , SubstanceBrightness);
         mySubstance.RenderAsync();
     }
 }
