5eaced314315ba644a9f71af597654f7559d05aa
ThomasVDev <thomasvgamedev@gmail.com>
2023-04-17 19:45:44 +0100

    BookSounds

--- Irony_App/Assets/Scripts/AlwaysPopUP.cs
+++ Irony_App/Assets/Scripts/AlwaysPopUP.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using DG.Tweening;
 using UnityEngine;
 
@@ -8,9 +9,11 @@ public class AlwaysPopUP : MonoBehaviour
 {
     [SerializeField] private Ease easeMode = Ease.InCirc;
     [SerializeField] private float duration = .25f;
+    [SerializeField] private AudioSource audio;
     private void OnEnable()
     {
         transform.localScale = new Vector3();
         transform.DOScale(Vector3.one, duration).SetEase(easeMode);
+        audio.Play();
     }
 }
